56833a745310ac6b789565f7fa64734810780216
Vadim Prokoshin <vpro85@gmail.com>
2021-09-20 00:54:51 +0200

    Configure DB Structure

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const {json} = require("sequelize");
+
 const PORT = process.env.PORT || 5000
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=> {
+    res.status(200).json({message: 'Working!'})
+})
 
 const start = async () => {
     try {
