19dbb57447e4fd0ebb0cba1d90503b197f5886f0
Vadim Prokoshin <vpro85@gmail.com>
2021-09-21 03:19:45 +0200

    Add check role (Admin or User)

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -1,9 +1,10 @@
 const Router = require("express")
 const router = new Router()
 const deviceController = require('../controllers/deviceController')
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post('/',deviceController.create)
-router.get('/',deviceController.getAll)
-router.get('/:id',deviceController.getOne)
+router.post('/', checkRole('ADMIN'), deviceController.create)
+router.get('/', deviceController.getAll)
+router.get('/:id', deviceController.getOne)
 
 module.exports = router
